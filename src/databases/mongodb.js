48071f099486a6ffae982ec4881ab05b61dcc462
talles-alexandre <alexandretalles13@gmail.com>
2022-12-01 10:20:46 -0300

    server create/db

---
+++ src/databases/mongodb.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let db;
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+
+try {
+  await mongoClient.connect();
+  db = mongoClient.db(process.env.MONGO_DB);
+  console.log("Banco de Dados conectado");
+} catch (error) {
+  console.log("Erro ao conectar o banco de dados");
+}
+
+db = mongoClient.db(process.env.MONGO_DB);
+
+export default db;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server running in port ${PORT}`);
+});
