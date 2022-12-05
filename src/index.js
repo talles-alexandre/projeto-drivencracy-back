eba7c0da31e453113b60a8ed428cd9e444c65c2e
talles-alexandre <alexandretalles13@gmail.com>
2022-12-05 01:04:20 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(pollRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running in port ${PORT}`);
